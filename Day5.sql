c2ed6e05206526255f5233c496a4db8a226b126c
Girish Sabale <112193285+B00mer10@users.noreply.github.com>
2025-11-07 13:53:31 -0300

    Add files via upload

---
+++ Day5.sql
@@ -0,0 +1,25 @@
+select * from patients;
+
+select * from services_weekly;
+
+select distinct service from services_weekly;
+
+select patient_id,name,age,satisfaction
+from patients 
+where service = 'surgery' AND satisfaction < 70;
+
+select service, week ,patients_refused ,patients_request
+from services_weekly
+order by patients_refused desc
+limit 5;
+
+SELECT patient_id, name, service, satisfaction
+FROM patients
+ORDER BY satisfaction DESC
+LIMIT 5 OFFSET 2;
+
+select 
+    sum(patients_admitted) as admitted,
+    sum(patients_refused) as refused,
+    round(avg(patient_satisfaction),2) as satisfaction
+from services_weekly;
